785e425d24614ae77e03b1d888a4a14c184d02d1
Marjan112 <marjanramadanovic1234@gmail.com>
2026-04-17 01:23:36 +0200

    add functions that dont belong anywhere in utils.rs

---
+++ src/utils.rs
@@ -0,0 +1,45 @@
+#![allow(dead_code)]
+
+use ratatui::{text::Line, style::Stylize};
+use chrono::{format::{DelayedFormat, StrftimeItems}, Local, TimeZone};
+
+use crate::env::CHATTY_VERSION;
+
+pub fn datetime_from_timestamp(secs: i64) -> DelayedFormat<StrftimeItems<'static>> {
+    Local.timestamp_opt(secs, 0)
+        .unwrap()
+        .format("%Y-%m-%d %H:%M")
+}
+
+pub fn greet_message() -> Vec<Line<'static>> {
+    vec![
+        Line::from(vec![
+            "Welcome to ".into(),
+            "ChaTTY ".yellow(),
+            CHATTY_VERSION.yellow(),
+            "!".into()
+        ]),
+        Line::from(vec![
+            "Use ".into(),
+            "UP".yellow(),
+            "/".into(),
+            "DOWN".yellow(),
+            " to scroll".into()
+        ]),
+        Line::from(vec![
+            "Type and press ".into(),
+            "ENTER".yellow(),
+            " to send".into()
+        ]),
+        Line::from(vec![
+            "Type ".into(),
+            "/help".yellow(),
+            " for help".into()
+        ]),
+        Line::from(vec![
+            "Press ".into(),
+            "ESC".yellow(),
+            " to exit".into()
+        ])
+    ]
+}
